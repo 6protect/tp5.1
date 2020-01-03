db2c3f964d6caf5ec9ba01a571a8f4a448df1362
liu <liu@qq.com>
2020-01-03 14:38:05 +0800

    第一次更改

---
+++ tp5.1/application/admin/controller/Admin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2020/1/3
+ * Time: 11:30
+ */
+
+namespace app\admin\controller;
+
+
+class Admin
+{
+    public function index(){
+
+    }
+}
---
+++ tp5.1/application/common/model/order.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2020/1/3
+ * Time: 11:53
+ */
+
+namespace app\common\model;
+
+use think\Model;
+
+class order extends Model
+{
+
+}
